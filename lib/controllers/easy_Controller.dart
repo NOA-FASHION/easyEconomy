7bb17e8c9d13ab76e627590b2ff8c97345a77fab
malmont <michel.almont@gmail.com>
2021-08-25 17:07:16 -0400

    first commit

---
+++ lib/controllers/easy_Controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class EasyController extends ChangeNotifier {
+  EasyController() {
+    _initEconomy();
+  }
+
+  void _initEconomy() async {}
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:easyeconomy/screens/homeEconomy.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'controllers/easy_Controller.dart';
+
+void main() => runApp(EasyEconomy());
+
+class EasyEconomy extends StatefulWidget {
+  @override
+  _EasyEconomyState createState() => _EasyEconomyState();
+}
+
+class _EasyEconomyState extends State<EasyEconomy> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      title: 'EasyEconomy',
+      home: ChangeNotifierProvider<EasyController>(
+          create: (context) => EasyController(), child: Home()),
+    );
+  }
+}
